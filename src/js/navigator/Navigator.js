8811c3b775a94e05d6b94c116d5b8ba44fce12a9
WarnerHooh <Warner.Hooh@gmail.com>
2017-01-05 15:47:39 +0800

    merge img file

--- src/js/navigator/Navigator.js
+++ src/js/navigator/Navigator.js
@@ -5,7 +5,7 @@ import { NavigatorIOS } from 'react-native'
 
 import IndexScene from '../scenes/Index'
 import Scanner from '../scenes/Scanner'
-import ScannerIcon from '../../../imgs/scannerIcon.png'
+import ScannerIcon from '../../image/scannerIcon.png'
 
 export default class extends Component {
   _toScanner() {
